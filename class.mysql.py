1689f297eca03c1e4ac79a2aade90e845aec8cd2
shgdym <shgdym@hotmail.com>
2019-09-27 22:38:28 +0800

    python mysql first commit

---
+++ class.mysql.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import pymysql
+from const import const
+
+__author__ = 'shgdym'
+
+class MySql:
+    def __init__(self, host=const.DB_HOST, user=const.DB_USER, password=const.DB_PASS, db=const.DB_NAME, port=const.DB_PORT):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.db = db
+        self.port = port
+        self.connect()
+
+    def connect(self):
+        if 'cursor' in locals().keys():
+            pass
+        else:
+            self.dbconn = pymysql.connect(host=self.host, user=self.user, password=self.password, db=self.db, port=self.port)
+            self.cursor = self.dbconn.cursor()
+
+    def getRows(self, query_sql):
+        self.cursor.execute(query_sql)
+        results = self.cursor.fetchall()
+        return results
+
+    def getFirstRow(self, query_sql):
+        # TODO add limit 1 to sql
+        res = self.getRows(query_sql)
+        return res[0]
+
+    def query(self, query_sql):
+        self.cursor.execute(query_sql)
+        self.dbconn.commit()
+
+    def close(self):
+        self.cursor.close()
+
+if __name__ == "__main__":
+    objMysql = MySql()
+    sql = """CREATE TABLE EMPLOYEE (
+             FIRST_NAME  CHAR(20) NOT NULL,
+             LAST_NAME  CHAR(20),
+             AGE INT,  
+             SEX CHAR(1),
+             INCOME FLOAT )"""
+    objMysql.query(sql)
+
+    sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
+             LAST_NAME, AGE, SEX, INCOME)
+             VALUES ('Mac', 'Mohan', 20, 'M', 2000),('john', 'Mohan', 22, 'W', 1000)"""
+    objMysql.query(sql)
+
+    sql = """SELECT *
+             FROM EMPLOYEE"""
+    res = objMysql.getRows(sql)
+    print(res)
+    exit()
---
+++ const.py
@@ -0,0 +1,22 @@
+class Const(object):
+    class ConstError(TypeError):
+        pass
+
+    class ConstCaseError(ConstError):
+        pass
+
+    def __setattr__(self, name, value):
+        if name in self.__dict__:
+            raise self.ConstError("Can't change const.%s" % name)
+        if not name.isupper():
+            raise self.ConstCaseError('const name "%s" is not all supercase' % name)
+
+        self.__dict__[name] = value
+
+const = Const()
+
+const.DB_HOST  = 'localhost'
+const.DB_USER = 'root'
+const.DB_PASS = ''
+const.DB_NAME = 'local'
+const.DB_PORT = 3306
